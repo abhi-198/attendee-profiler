bb7b97234850b4ce46dffdeffeb437c30e2d96a3
abhi-198 <43150958+abhi-198@users.noreply.github.com>
2018-12-01 21:26:27 +0530

    modified for training model

--- trainerPage.py
+++ trainerPage.py
@@ -6,7 +6,7 @@ Created on Sat Dec  1 13:18:41 2018
 """
 
 from tkinter import *
-import trainner
+import trainer
 
 
 def main_screen():
@@ -25,27 +25,27 @@ def main_screen():
     section.set("A")
     Label(text = "Welcome to FRAT Trainer", bg = "peach puff", width = "300", height = "2", font = ("Calibri", 11), fg = "SeaGreen3").pack()
     Label(text = "", bg = 'floral white').pack()
-    Label(text = "Course", bg = 'floral white', font = ("Calibri", 10), fg = 'hot pink').pack()
+    Label(text = "Course", bg = 'floral white', font = ("Times", "10", "bold"), fg = 'hot pink').pack()
     m = OptionMenu(screen, course, "B.Tech", "BBA", "BCA", "MBA", "Diploma")
     m.configure(bg = 'SeaGreen1')
     m.pack()
     Label(text = "", bg = 'floral white').pack()
-    Label(text = "Branch", bg = 'floral white', font = ("Calibri", 10), fg = 'hot pink').pack()
+    Label(text = "Branch", bg = 'floral white', font = ("Times", "10", "bold"), fg = 'hot pink').pack()
     a = OptionMenu(screen, branch, "CSE", "ME", "EC", "EN","CE")
     a.configure(bg = 'SeaGreen1')
     a.pack()
     Label(text = "", bg = 'floral white').pack()
-    Label(text = "Year", bg = 'floral white', font = ("Calibri", 10), fg = 'hot pink').pack()
+    Label(text = "Year", bg = 'floral white', font = ("Times", "10", "bold"), fg = 'hot pink').pack()
     w = OptionMenu(screen, year, "First", "Second", "Third", "Fourth")
     w.configure(bg = 'SeaGreen1')
     w.pack()
     Label(text = "", bg = 'floral white').pack()
-    Label(text = "Section", bg = 'floral white', font = ("Calibri", 10), fg = 'hot pink').pack()
+    Label(text = "Section", bg = 'floral white', font = ("Times", "10", "bold"), fg = 'hot pink').pack()
     b = OptionMenu(screen, section, "A", "B", "C", "D", "E")
     b.configure(bg = 'SeaGreen1')
     b.pack()
     Label(text = "", bg = 'floral white').pack()
-    Button(text = "Start Scanning", fg = "dark violet", bg = "SeaGreen1", height = "2", width = "30", command = faceRec.face_rec(str(year.get()),str(course.get()),str(branch.get()),str(section.get())).pack())
+    Button(text = "Create training model", fg = "dark violet", bg = "SeaGreen1", height = "2", width = "30", command = lambda : trainer.train(str(year.get()),str(course.get()),str(branch.get()),str(section.get()))).pack()
     
     screen.mainloop()
 
