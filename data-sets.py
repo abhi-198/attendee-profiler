ae2919f7b47777ce409bce25b5ebd9aabdb81012
lakshaydeepak <lakshaydeepak@gmail.com>
2018-12-04 18:05:48 +0530

    Create data-sets.py

---
+++ data-sets.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 18:04:24 2018
+
+@author: laksh
+"""
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('E:\\A\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)   
+count=0
+while True:
+    ret, frame = cap.read() 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
