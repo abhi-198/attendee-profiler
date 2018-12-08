93542f6704e19a8b46aef520578b24cccc57220a
abhi-198 <43150958+abhi-198@users.noreply.github.com>
2018-12-08 17:18:54 +0530

    minior bug fixing

--- generateDataSet.py
+++ generateDataSet.py
@@ -1,14 +1,17 @@
 import cv2 
 import numpy as np
 import urllib
+from tkinter import messagebox
+import tkinter as tk
 
 url='http://192.168.201.2:8080/shot.jpg'
 
-def generate():
+def generate(screen=None):
     Id=0
+    screen=tk.Tk()
     eye_cascade=cv2.CascadeClassifier('C:\\Users\\Abhishek\\Anaconda3\\pkgs\\libopencv-3.4.1-h875b8b8_3\\Library\\etc\\haarcascades\\haarcascade_eye.xml')
     face_cascade =cv2.CascadeClassifier('C:\\Users\\Abhishek\\Anaconda3\\pkgs\\libopencv-3.4.1-h875b8b8_3\\Library\\etc\\haarcascades\\haarcascade_frontalface_default.xml')    
-    print("Starting Creating Face Recognition dataset" )
+    messagebox.showinfo("Generate DataSet","Starting Creating Face Recognition dataset" )
     while(True):
         imgResponse = urllib.request.urlopen(url)
  
@@ -34,12 +37,13 @@ def generate():
             
             for ex,ey,ew,eh in eyes:
                 cv2.rectangle(img[y:y+h,x:x+w],(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        cv2.imshow("camera",img)
+        cv2.imshow("Generate Data Set",img)
         if cv2.waitKey(int(1000/12)) & 0xff==ord("q"):
             break
         
     cv2.destroyAllWindows()
-    print("Dataset is created successfully and saved at C:\\Users\\Abhishek\\local data")
+    messagebox.showinfo("Generate DataSet","Dataset is created successfully and saved at C:\\Users\\Abhishek\\local data")
+    screen.destroy()
     
 if __name__=="__main__":
     generate()
